1c1000981bbc689f29ef20fa8ba9a8bfe11778ee
Roman <roman.berti09@gmail.com>
2022-03-24 12:23:57 +0100

    Arm motor controler  efinitons without definit pinout
    Signed-off-by: Roman <roman.berti09@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 /*this Program acts as a prototype. it dosn't include any PWM modulation nor cours-correcten.
 It is built as Step after Step programm, if any step fails the Program feils it shoud proof the concept of the pland roboter
 */
-
-
 #include "AnalogIn.h"
 #include "DigitalIn.h"
 #include "DigitalOut.h"
@@ -17,10 +15,7 @@ It is built as Step after Step programm, if any step fails the Program feils it 
 //arbitrary allocation of pins -> for testing this has to be adjusted
 AnalogIn ch0(PA_15); //IR TOF sensor at the front
 AnalogIn RearSensor(PC_9); //IR TOF sensor at the back facing down
-DigitalOut FleftMotor(PC_10);
-DigitalOut FrightMotor(PC_11);
-DigitalOut BrightMotor(PB_15);
-DigitalOut BleftMotor(PB_14);
+
 DigitalOut armMotor(PC_12);
 // main() runs in its own thread in the OS
     // Enable the ADC, 12MHz,  the final term should be (2**BURST_CHANNELS - 1)
@@ -29,10 +24,11 @@ float trigerValueRearSensor = 0.0f;
 float trigerValueFrontSensor = 0.0f;
 
 
-//actors Definition
+//-----------------------------------------actors Definition----------------------------------------------------
 
 //Encoder pin 
-EncoderCounter  encoder_M3(PA_0, PA_1);
+EncoderCounter  encoder_M_right(PA_0, PA_1);
+EncoderCounter  encoder_M_left(PA_2, PA_3);  //Pin zuweisung Provisorisch!!!
 
 //motor pin
 FastPWM pwm_M_right(PA_10);              
@@ -45,10 +41,14 @@ float kn = 180.0f / 12.0f;                  // define motor constant in rpm per 
 float k_gear = 100.0f / 78.125f;            // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
 float kp = 0.1f;                            // define custom kp, this is the default speed controller gain for gear box 78.125:1
 
-PositionController positionController_M_right(counts_per_turn * k_gear, kn / k_gear, kp * k_gear, max_voltage, pwm_M_right, encoder_M3); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
-PositionController positionController_M_left(counts_per_turn * k_gear, kn / k_gear, kp * k_gear, max_voltage, pwm_M_left, encoder_M3); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
+//motors for tracks
+PositionController positionController_M_right(counts_per_turn * k_gear, kn / k_gear, kp * k_gear, max_voltage, pwm_M_right, encoder_M_right); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
+PositionController positionController_M_left(counts_per_turn * k_gear, kn / k_gear, kp * k_gear, max_voltage, pwm_M_left, encoder_M_left); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
 
+//Arm Motor
+PositionController positionController_M_Arm(counts_per_turn * k_gear, kn / k_gear, kp * k_gear, max_voltage, pwm_M_right, encoder_M_right); // parameters adjusted to 100:1 gear, we need a different speed controller gain here
 
+//-----------------------------------------Functions----------------------------------------------------------
 //only moves the arm in to the starting position
 int StartPosition(void){
     if(true){
@@ -58,7 +58,7 @@ int StartPosition(void){
 }
 
 //Drives forward into the next step
-int DriveForward(int8_t dist){
+int Drive(int8_t dist){
     int8_t i = 0;         //prov condition variable
 
 
@@ -75,10 +75,15 @@ int DriveForward(int8_t dist){
 }
 
 //only turns the arm until the robot is on the next step
-int LiftUp(void){
-    if (true) {
-    return 1;
-    }
+//not yet clear if the motor controler function drives to a absolute poition or if it drives the given distance relative to the current position
+int LiftUp(int8_t deg){
+    int8_t rotation = deg;
+    int8_t i = 0;         //prov condition variable
+    do{
+        positionController_M_right.setDesiredRotation(deg);
+        
+        i++;
+    }while(i < 1);
     return 0;
 }
 
@@ -90,11 +95,11 @@ int main()
     while (true) {
         ToNextFunction = StartPosition();
         if(NULL != ToNextFunction){
-            ToNextFunction = DriveForward();
+            ToNextFunction = Drive(3);
             if(NULL != ToNextFunction){
-                ToNextFunction = LiftUp();
+                ToNextFunction = LiftUp(5);
                 if(NULL != ToNextFunction){
-                    ToNextFunction = DriveBack();
+                    ToNextFunction = Drive(-1);
 
                 }
             }
